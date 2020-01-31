682d2f159310dc4a397ed84a9c1213a5c0893167
0zzycodes <horlalekan123@gmail.com>
2020-01-31 20:33:37 +0100

    Added map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,99 @@
-import React from 'react';
-import Map from './components/map/map';
+import React, { Component } from 'react';
+import MapGL, {
+  Popup,
+  NavigationControl,
+  FullscreenControl
+} from 'react-map-gl';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
-}
+import Pins from './components/map/shop-indicator';
+import CityInfo from './components/map/shop-info';
 
-export default App;
+import CITIES from './components/data/cities.json';
+
+const TOKEN =
+  'pk.eyJ1Ijoib3p6eWNvZGUiLCJhIjoiY2s2MXhpbmdmMDdwejNrbW14eXJvaTYxayJ9.lwAX0SNCShN0GZqtmuLrmw';
+
+const fullscreenControlStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+
+const navStyle = {
+  position: 'absolute',
+  top: 36,
+  left: 0,
+  padding: '10px'
+};
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 37.785164,
+        longitude: -100,
+        zoom: 3.5,
+        bearing: 0,
+        pitch: 0
+      },
+      popupInfo: null
+    };
+  }
+
+  _updateViewport = viewport => {
+    this.setState({ viewport });
+  };
+
+  _onClickMarker = city => {
+    this.setState({ popupInfo: city });
+  };
+
+  _renderPopup() {
+    const { popupInfo } = this.state;
+
+    return (
+      popupInfo && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={popupInfo.longitude}
+          latitude={popupInfo.latitude}
+          closeOnClick={false}
+          onClose={() => this.setState({ popupInfo: null })}
+        >
+          <CityInfo info={popupInfo} />
+        </Popup>
+      )
+    );
+  }
+
+  render() {
+    const { viewport } = this.state;
+
+    return (
+      <MapGL
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        // mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onViewportChange={this._updateViewport}
+        mapboxApiAccessToken={TOKEN}
+      >
+        <Pins data={CITIES} onClick={this._onClickMarker} />
+
+        {this._renderPopup()}
+
+        <div style={fullscreenControlStyle}>
+          <FullscreenControl />
+        </div>
+        <div style={navStyle}>
+          <NavigationControl />
+        </div>
+      </MapGL>
+    );
+  }
+}
